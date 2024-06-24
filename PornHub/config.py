78dbeaf41ae2eebe0a7660e6569fee4e2c425824
rohitt99 <164199173+rohitt99@users.noreply.github.com>
2024-06-25 01:29:39 +0530

    Update config.py

--- PornHub/config.py
+++ PornHub/config.py
@@ -1,12 +1,12 @@
 from typing import List
 
-API_ID: int = insert_your_own_api_id_here
-API_HASH: str = "insert_your_own_api_hash_here"
-TOKEN: str = "insert_your_bot_token_here"
+API_ID: int = 28717614
+API_HASH: str = "e9c14c08186affad9169f8840b456273"
+TOKEN: str = "7446456137:AAEAunQ4YOJEdoeqR2MQuqn0KBvp8grQcGA"
 
-log_chat: int = -1001662591986
-sub_chat: str = "SakuraEmpireTeam"
-sudoers: List[int] = [2137482758, 1249591948]
+log_chat: int = -1001977784654
+sub_chat: str = "IMPERILMENT_SUPPORT"
+sudoers: List[int] = [6472508204, 1249591948]
 prefixs: List[str] = ["/", "!", ".", "$", "-"]
 
 # notes
